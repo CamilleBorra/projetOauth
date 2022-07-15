05a9dbb00de94a24279786e8ce31100a9f78f1cb
CamilleBorra <cborra@hotmail.fr>
2022-07-15 17:12:22 +0200

    gitignore
    Signed-off-by: CamilleBorra <cborra@hotmail.fr>

---
+++ client/conf.exemple.php
@@ -0,0 +1,12 @@
+<?
+define('OAUTH_CLIENT_ID', '');
+define('OAUTH_CLIENT_SECRET', '');
+define('FACEBOOK_CLIENT_ID', '');
+define('FACEBOOK_CLIENT_SECRET', '');
+
+define('DISCORD_CLIENT_ID', '');
+define('DISCORD_CLIENT_SECRET', '');
+
+
+define('GIT_CLIENT_ID', '' );
+define('GIT_CLIENT_SECRET', '');
